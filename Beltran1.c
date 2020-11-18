62bcc84ff0415c97d97b2e2134b6967e7ff3a282
B13ckp <74647375+B13ckp@users.noreply.github.com>
2020-11-18 01:00:28 -0700

    Add files via upload

---
+++ Beltran1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	float a;
+     printf("digite un numero: ");
+	 scanf("%f",&a);
+	 
+	 printf("\nEl numero que digito es: %.2f",a);
+	
+	
+	return 0;
+}
